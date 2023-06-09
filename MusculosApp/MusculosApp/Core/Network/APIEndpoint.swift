3171fee9124246bc11382407b1a6bfa4b9e65ead
Alexandru <calinbercea007@gmail.com>
2023-06-10 01:45:18 +0300

    Create authentication helper

---
+++ MusculosApp/MusculosApp/Core/Network/APIEndpoint.swift
@@ -0,0 +1,22 @@
+//
+//  APIEndpoint.swift
+//  MusculosApp
+//
+//  Created by Solomon Alexandru on 10.06.2023.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case authentication = "/auth/login"
+    case register       = "/users"
+    case persons        = "/persons"
+}
+
+public class APIEndpoint {
+    static let base = "http://0.0.0.0:3000"
+    
+    static func baseWithEndpoint(endpoint: Endpoint) -> String {
+        return Self.base + endpoint.rawValue
+    }
+}
---
+++ MusculosApp/MusculosApp/Core/UserDefaultsWrapper.swift
@@ -0,0 +1,40 @@
+//
+//  UserDefaultsWrapper.swift
+//  MusculosApp
+//
+//  Created by Solomon Alexandru on 10.06.2023.
+//
+
+import Foundation
+
+
+public class UserDefaultsWrapper: NSObject {
+    private static var sharedWrapper: UserDefaultsWrapper = {
+        return UserDefaultsWrapper()
+    }()
+    
+    class var shared: UserDefaultsWrapper {
+        get {
+            return self.sharedWrapper
+        }
+        set {
+            self.sharedWrapper = newValue
+        }
+    }
+    
+    private let userDefaults: UserDefaults
+
+    private let tokenKey = "AuthToken"
+    var authToken: String? {
+        get {
+            return self.userDefaults.string(forKey: self.tokenKey)
+        }
+        set {
+            self.userDefaults.set(newValue, forKey: self.tokenKey)
+        }
+    }
+    
+    private override init() {
+        self.userDefaults = UserDefaults.standard
+    }
+}
