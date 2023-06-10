afccf6404883d3210f8a740c56501ded5f0b364b
Alexandru <calinbercea007@gmail.com>
2023-06-10 20:50:32 +0300

    Create AuthenticationHelper

--- MusculosApp/MusculosApp/View/LoginView.swift
+++ MusculosApp/MusculosApp/View/LoginView.swift
@@ -16,66 +16,73 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
-            Text("Welcome")
-                .foregroundColor(.white)
-                .padding([.top, .bottom], 50)
-                .shadow(radius: 6.0, x: 10, y: 10)
-            
-            Image("profile-img")
-                .resizable()
-                .frame(width: 180, height: 180)
-                .clipShape(Circle())
-                .overlay(Circle())
-                .shadow(radius: 9.0, x: 20, y: 10)
-                .padding(.bottom, 40)
-            
-            VStack(alignment: .leading, content: {
-                TextField("Username", text: $viewModel.username)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(25.0)
-                    .shadow(radius: 10.0, x: 5, y: 10)
-                SecureField("Password", text: $viewModel.password)
-                    .textContentType(.password)
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(25.0)
-                    .shadow(radius: 10.0, x: 5, y: 10)
-            })
-            .padding([.leading, .trailing], 50)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("Forgot password?")
-                    .padding(.leading, 150)
+            if self.viewModel.isLoading {
+                ProgressView("Loading...")
+            } else {
+                Text("Welcome")
                     .foregroundColor(.white)
-            })
-            
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("Sign In")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 200, height: 60)
-                    .background(Color(.orange))
-                    .cornerRadius(20.0)
-                    .shadow(radius: 10.0, x: 20, y: 10)
-            })
-            .opacity(self.buttonOpacity)
-            .disabled(!viewModel.isFormValid)
-            .padding(.top, 50)
-            
-            Spacer()
-            HStack {
-                Text("Don't have an account?")
-                    .foregroundColor(.white)
+                    .padding([.top, .bottom], 50)
+                    .shadow(radius: 6.0, x: 10, y: 10)
                 
+                Image("profile-img")
+                    .resizable()
+                    .frame(width: 180, height: 180)
+                    .clipShape(Circle())
+                    .overlay(Circle())
+                    .shadow(radius: 9.0, x: 20, y: 10)
+                    .padding(.bottom, 40)
+                
+                VStack(alignment: .leading, content: {
+                    TextField("Username", text: self.$viewModel.username)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .background(Color(.white))
+                        .cornerRadius(25.0)
+                        .shadow(radius: 10.0, x: 5, y: 10)
+                    SecureField("Password", text: self.$viewModel.password)
+                        .textContentType(.password)
+                        .padding()
+                        .background(Color(.white))
+                        .cornerRadius(25.0)
+                        .shadow(radius: 10.0, x: 5, y: 10)
+                })
+                .padding([.leading, .trailing], 50)
+                Button(action: {}, label: {
+                    Text("Forgot password?")
+                        .padding(.leading, 150)
+                        .foregroundColor(.white)
+                })
+                
+                Button(action: {
+                    self.viewModel.authenticateUser()
+                }, label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 200, height: 60)
+                        .background(Color(.orange))
+                        .cornerRadius(20.0)
+                        .shadow(radius: 10.0, x: 20, y: 10)
+                })
+                .opacity(buttonOpacity)
+                .disabled(!self.viewModel.isFormValid)
+                .padding(.top, 50)
+                
+                Spacer()
+                HStack {
+                    Text("Don't have an account?")
+                        .foregroundColor(.white)
+                }
             }
         }
+        .onAppear(perform: {
+            viewModel.setupSubscriptions()
+        })
     }
-    
     var buttonOpacity: Double {
-        return viewModel.isFormValid ? 1 : 0.5
+        return self.viewModel.isFormValid ? 1 : 0.5
     }
 }
 
