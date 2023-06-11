58bda4cbc62504bb47320170d3b2effc8d284bb8
Alexandru <calinbercea007@gmail.com>
2023-06-11 18:14:03 +0300

    Handle signup

---
+++ MusculosApp/MusculosApp/Core/Network/Request/LoginRequest.swift
@@ -0,0 +1,26 @@
+//
+//  LoginRequest.swift
+//  MusculosApp
+//
+//  Created by Solomon Alexandru on 11.06.2023.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    var token: String
+}
+
+struct LoginRequest: Request {
+    var headers: [String : String]?
+    
+    typealias ReturnType = LoginResponse
+    
+    var path: String = APIEndpoint.baseWithEndpoint(endpoint: .authentication)
+    var method: HTTPMethod = .post
+    var body: [String: Any]
+    
+    init(body: [String: Any]) {
+        self.body = body
+    }
+}
