22e70ca7b208ebebf9b85b3d403292cf10aae6b0
Alexandru <calinbercea007@gmail.com>
2023-07-09 16:24:49 +0300

    [create-workout-feed] Create initial view

---
+++ MusculosApp/MusculosApp/Workout/Workout Feed/WorkoutFeedView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutFeedView.swift
+//  MusculosApp
+//
+//  Created by Solomon Alexandru on 09.07.2023.
+//
+
+import SwiftUI
+
+struct WorkoutFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutFeedView()
+}
