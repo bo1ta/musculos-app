d5557cc959c9d42131eb3a9e890d9b7a601155be
Alexandru <calinbercea007@gmail.com>
2023-07-11 22:57:32 +0300

    [create-workout-feed] update spacing

--- MusculosApp/MusculosApp/Workout/Workout Feed/Components/MiniCard.swift
+++ MusculosApp/MusculosApp/Workout/Workout Feed/Components/MiniCard.swift
@@ -55,10 +55,7 @@ struct MiniCard: View {
 
 struct CardItem_Preview: PreviewProvider {
     static var previews: some View {
-        MiniCard(item: MiniCardItem(title: "Featured workout",
-                                        subtitle: "Gym workout",
-                                        description: "Body contouring",
-                                        iconPillOption: IconPillOption(title: "In progress"))
-        )
+        MiniCard(item: MiniCardItem(title: "Featured workout", subtitle: "Gym workout", description: "Body contouring", iconPillOption: IconPillOption(title: "In progress")))
+        .previewLayout(.sizeThatFits)
     }
 }
