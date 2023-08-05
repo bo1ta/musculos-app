38089214d7c0082a65767a3a2bc7858b0dabe5a1
bo1ta <calinbercea007@gmail.com>
2023-08-05 22:22:36 +0300

    [custom-tab-bar] clean up

--- MusculosApp/MusculosApp/Core/View Components/Navigation/CustomTabBar/CustomTabBarView.swift
+++ MusculosApp/MusculosApp/Core/View Components/Navigation/CustomTabBar/CustomTabBarView.swift
@@ -19,18 +19,42 @@ struct CustomTabBarView: View {
     
     var body: some View {
         HStack {
+            Spacer()
             ForEach(tabBarItems.indices, id: \.self) { index in
-                Spacer()
-                tabItem(with: tabBarItems[index], isSelected: selectedIndex == index) {
-                    withAnimation(.snappy) {
-                        selectedIndex = index
+                tabItem(with: tabBarItems[index], isSelected: selectedIndex == tabBarItems[index].rawValue) {
+                    withAnimation(.easeIn(duration: 0.1)) {
+                        selectedIndex = tabBarItems[index].rawValue
                     }
                 }
-                Spacer()
             }
+            Spacer()
         }
         .frame(height: 70)
         .background(Color(.systemGray6))
         .cornerRadius(25)
+    }
+}
+
+extension CustomTabBarView {
+    private func tabItem(with item: TabBarItem, isSelected: Bool, onTapGesture: @escaping () -> Void) -> some View {
+        VStack {
+            if item == .add {
+                AddTabBarButton(onTapGesture: onTapGesture)
+                    .padding(.bottom)
+            } else {
+                Image(systemName: item.imageName)
+                    .foregroundStyle(isSelected ? .black : .gray)
+                    .onTapGesture(perform: onTapGesture)
+                    .frame(height: 30)
+                    .font(Font(CTFont(.menuItem, size: 23)))
+                    .fontWeight(.bold)
+            }
+        }
+    }
+}
+
+struct CustomTabBarView_Preview: PreviewProvider {
+    static var previews: some View {
+        CustomTabBarView(tabBarItems: [.dashboard, .workout, .add], selectedIndex: Binding<String>.constant(""))
     }
 }
