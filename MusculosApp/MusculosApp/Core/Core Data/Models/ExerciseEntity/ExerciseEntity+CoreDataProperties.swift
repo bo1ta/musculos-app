beac0fe3785ddc6d388054b8a76062f07cd85d52
bo1ta <calinbercea007@gmail.com>
2024-04-07 17:15:51 +0300

    update filepaths

--- MusculosApp/MusculosApp/Core/Core Data/Models/ExerciseEntity/ExerciseEntity+CoreDataProperties.swift
+++ MusculosApp/MusculosApp/Core/Core Data/Models/ExerciseEntity/ExerciseEntity+CoreDataProperties.swift
@@ -29,6 +29,49 @@ extension ExerciseEntity {
   
 }
 
-extension ExerciseEntity : Identifiable {
+extension ExerciseEntity : Identifiable {}
+
+// MARK: - ReadOnlyConvertible impl
+
+extension ExerciseEntity: ReadOnlyConvertible {
+  func toReadOnly() -> Exercise {
+    return Exercise(
+      category: self.category ?? "",
+      equipment: self.equipment,
+      id: self.exerciseId!,
+      level: self.level!,
+      name: self.name!,
+      primaryMuscles: self.primaryMuscles,
+      secondaryMuscles: self.secondaryMuscles,
+      instructions: self.instructions!,
+      imageUrls: self.imageUrls!
+    )
+  }
   
+  func update(with entity: Exercise) { }
+}
+
+// MARK: - Common Predicate
+
+extension ExerciseEntity {
+  enum CommonPredicate {
+    case byId(UUID)
+    case isFavorite
+    
+    var nsPredicate: NSPredicate {
+      switch self {
+      case .byId(let uuid):
+        return NSPredicate(
+          format: "%K == %@",
+          #keyPath(ExerciseEntity.exerciseId),
+          uuid as NSUUID
+        )
+      case .isFavorite:
+        return NSPredicate(
+          format: "%K == true",
+          #keyPath(ExerciseEntity.isFavorite)
+        )
+      }
+    }
+  }
 }
