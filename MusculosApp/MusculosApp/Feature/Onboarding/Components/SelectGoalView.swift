c3901966f6bdc62fede9b996f7cc08ea36189790
bo1ta <calinbercea007@gmail.com>
2024-06-19 01:16:34 +0300

    remove unused method

--- MusculosApp/MusculosApp/Feature/Onboarding/Components/SelectGoalView.swift
+++ MusculosApp/MusculosApp/Feature/Onboarding/Components/SelectGoalView.swift
@@ -22,51 +22,6 @@ struct SelectGoalView: View {
     }
     .padding(20)
   }
-  
-  private func createGoalCard(_ goal: OnboardingData.Goal) -> some View {
-    let isSelected = selectedGoal == goal
-    let color: Color = isSelected ? Color.AppColor.blue500 : .black
-    
-    return Button {
-      selectedGoal = goal
-    } label: {
-      
-      RoundedRectangle(cornerRadius: 25)
-        .frame(maxWidth: .infinity)
-        .frame(height: 130)
-        .foregroundStyle(.white)
-        .shadow(color: isSelected ? Color.AppColor.blue500 : .gray, radius: 3)
-        .overlay {
-          HStack {
-            VStack(alignment: .leading, spacing: 5) {
-              Text(goal.title)
-                .font(.custom("Roboto-Bold", size: 20))
-                .foregroundStyle(color)
-              Text(goal.description)
-                .font(.custom("Roboto-Regular", size: 15))
-                .foregroundStyle(color)
-            }
-            Spacer()
-            
-            if !isSelected {
-              goal.image
-                .font(.system(size: 25))
-                .foregroundStyle(color)
-            } else {
-              Circle()
-                .frame(width: 35, height: 35)
-                .foregroundStyle(Color.AppColor.blue500)
-                .overlay {
-                  Image(systemName: "checkmark")
-                    .foregroundStyle(.white)
-                }
-            }
-          }
-          .padding(.horizontal, 20)
-        }
-    }
-    .scaleEffect(isSelected ? 0.98 : 1.0)
-  }
 }
 
 #Preview {
