659639262b6ce6bb604dc6af369982b03c340cfe
bo1ta <calinbercea007@gmail.com>
2024-10-01 23:24:04 +0300

    Update TabView

---
+++ MusculosApp/MusculosApp/Main/App/AppTabView.swift
@@ -0,0 +1,37 @@
+//
+//  AppTabView.swift
+//  MusculosApp
+//
+//  Created by Solomon Alexandru on 04.06.2023.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+  var body: some View {
+    TabView {
+      HomeView()
+        .tabItem {
+          Label("Home", systemImage: "house")
+        }
+        .tag(0)
+
+      ExploreExerciseView()
+        .tabItem {
+          Label("Explore", systemImage: "newspaper")
+        }
+        .tag(1)
+
+      OverviewView()
+        .tabItem {
+          Label("Overview", systemImage: "person")
+        }
+        .tag(2)
+    }
+  }
+}
+
+#Preview {
+  AppTabView()
+}
+
