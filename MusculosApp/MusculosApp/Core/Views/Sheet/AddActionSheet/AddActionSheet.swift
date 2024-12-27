99fe82d3a14b194c916d434209e64c9a8333455a
bo1ta <calinbercea007@gmail.com>
2024-12-27 23:37:20 +0200

    Update AddActionSheet

--- MusculosApp/MusculosApp/Core/Views/Sheet/AddActionSheet/AddActionSheet.swift
+++ MusculosApp/MusculosApp/Core/Views/Sheet/AddActionSheet/AddActionSheet.swift
@@ -7,80 +7,55 @@
 
 import Foundation
 import SwiftUI
+import Components
+import Utility
 
 struct AddActionSheet: View {
   @Environment(\.dismiss) private var dismiss
-  
+
   let onItemTapped: (ItemType) -> Void
-  
+
+  var cardGradient = LinearGradient(colors: [.blue], startPoint: .top, endPoint: .bottom)
+
   var body: some View {
     VStack {
-      topBar
-        .padding(.horizontal, 20)
-      
-      LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], alignment: .center, content: {
-        ForEach(ItemType.allCases, id: \.self) { itemType in
-          /// Since it's a single card, it should be centered.
-          /// pass an offset
-          if itemType == .goal {
-            sheetCardItem(itemType)
-              .opacity(0.8)
-              .offset(x: 100)
-          } else {
-            sheetCardItem(itemType)
-              .opacity(0.8)
-          }
-        }
-      })
-    }
-  }
-  
-  private var topBar: some View {
-    HStack {
       Text("Create new")
-        .font(.header(.bold, size: 23))
+        .font(AppFont.poppins(.bold, size: 15.0))
         .foregroundStyle(.black)
-      Spacer()
-      Button(action: {
-        dismiss()
-      }, label: {
-        Image(systemName: "xmark")
-          .font(.system(size: 18))
-          .foregroundStyle(.gray)
-      })
+
+      ForEach(ItemType.allCases, id: \.self) { itemType in
+        Button(
+          action: { onItemTapped(itemType) },
+          label: {
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(itemType.cardColor)
+              .frame(maxWidth: .infinity)
+              .frame(height: 40)
+              .padding(.horizontal, 40)
+              .overlay {
+                HStack {
+                  Image(systemName: itemType.systemImageName)
+                    .foregroundStyle(.white)
+                  Text(itemType.title)
+                    .foregroundStyle(.white)
+                    .font(AppFont.poppins(.semibold, size: 14))
+                    .shadow(radius: 1.0)
+                  Spacer()
+                }
+                .padding(.leading, 60)
+              }
+          })
+      }
     }
-  }
-  
-  private func sheetCardItem(_ itemType: ItemType) -> some View {
-    Button(action: {
-      onItemTapped(itemType)
-    }, label: {
-      RoundedRectangle(cornerRadius: 24)
-        .frame(height: 100)
-        .frame(maxWidth: .infinity)
-        .foregroundStyle(itemType.cardColor)
-        .overlay {
-          VStack(alignment: .center) {
-            Image(systemName: itemType.systemImageName)
-              .foregroundStyle(itemType.imageColor)
-              .font(.system(size: 20))
-            
-            Text(itemType.title)
-              .font(.body(.regular, size: 17))
-              .foregroundStyle(.black)
-              .padding(.top, 10)
-          }
-        }
-    })
   }
 }
 
 extension AddActionSheet {
   enum ItemType: String, CaseIterable {
     case workout, exercise, goal
-    
+
     var title: String { self.rawValue.capitalized }
-    
+
     var systemImageName: String {
       switch self {
       case .workout: "list.dash.header.rectangle"
@@ -88,20 +63,12 @@ extension AddActionSheet {
       case .goal: "star"
       }
     }
-    
+
     var cardColor: Color {
       switch self {
-      case .workout: Color.AppColor.blue200
-      case .exercise: Color.AppColor.green500
+      case .workout: Color.orange
+      case .exercise: AppColor.navyBlue
       case .goal: Color.yellow
-      }
-    }
-    
-    var imageColor: Color {
-      switch self {
-      case .workout: Color.AppColor.blue500
-      case .exercise: Color.AppColor.green700
-      case .goal: Color.black
       }
     }
   }
--- MusculosApp/MusculosApp/Core/Views/Sheet/AddActionSheet/AddActionSheetContainer.swift
+++ MusculosApp/MusculosApp/Core/Views/Sheet/AddActionSheet/AddActionSheetContainer.swift
@@ -6,35 +6,38 @@
 //
 
 import SwiftUI
+import Components
 
 struct AddActionSheetContainer: View {
   private enum Step {
     case createItem, createWorkout, createExercise, createGoal
   }
-  
+
   @State private var currentStep: Step = .createItem
-  
+
   var body: some View {
-    VStack {
-      switch currentStep {
-      case .createItem:
-        AddActionSheet(onItemTapped: handleTap(for:))
-          .presentationDetents([.height(300)])
-          .transition(.asymmetric(insertion: .push(from: .top), removal: .push(from: .bottom)))
-      case .createWorkout:
-        AddWorkoutSheet(onBack: handleBack)
-          .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
-      case .createExercise:
-        AddExerciseSheet(onBack: handleBack)
-          .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
-      case .createGoal:
-        AddGoalSheet()
-          .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
+    ZStack(alignment: .top) {
+      VStack {
+        switch currentStep {
+        case .createItem:
+          AddActionSheet(onItemTapped: handleTap(for:))
+            .presentationDetents([.height(200)])
+            .transition(.asymmetric(insertion: .push(from: .top), removal: .push(from: .bottom)))
+        case .createWorkout:
+          AddWorkoutSheet(onBack: handleBack)
+            .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
+        case .createExercise:
+          AddExerciseSheet(onBack: handleBack)
+            .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
+        case .createGoal:
+          AddGoalSheet()
+            .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .push(from: .top)))
+        }
       }
+      .animation(.easeInOut(duration: 0.2), value: currentStep)
     }
-    .animation(.easeInOut(duration: 0.2), value: currentStep)
   }
-  
+
   @MainActor
   private func handleTap(for itemType: AddActionSheet.ItemType) {
     switch itemType {
